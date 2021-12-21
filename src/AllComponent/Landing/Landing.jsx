41663d32973e715ae61c1e6ddf43bc8e72e334cc
Mohammad Daniyal <94825634+daniyal3239@users.noreply.github.com>
2021-12-21 14:56:06 +0530

    updated

--- src/AllComponent/Landing/Landing.jsx
+++ src/AllComponent/Landing/Landing.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {NavigationBar} from "../NavigationSection/NavigationBar";
 export const Landing = () => {
     return (
-        <div>
-            <NavigationBar/>
-        </div>
+        <>//this will also work
+            <Navigationbar/>
+        </>
     )
 };
 
